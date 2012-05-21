7b2ab0c3c3662c50112863d41da940bc0dbbaf21
Winston Chang <winston@stdout.org>
2012-05-21 03:19:16 -0500

    Add font files

---
+++ R/fontcm.r
@@ -0,0 +1,10 @@
+#' fontcm package
+#'
+#' This package contains the Computer Modern font with Paul Murrell's
+#' symbol extensions. It is meant to be used with the extrafont package.
+#'
+#'
+#' @name fontcm
+#' @docType package
+#' @aliases fontcm package-fontcm
+NULL
